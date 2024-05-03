0e374d58a5ec8b19d5d8fced687fc9af5e6abf81
RaikOle <Raikolekannenberg@outlook.de>
2024-05-03 10:44:48 +0200

    Update App.js

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ export default function App() {
             );
           },
           tabBarShowLabel: true,
-          headerShown: false // Versteckt den Header komplett
+          headerShown: false
         })}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
