6ef2151e7983145c246c10015e5e19e8daf95d95
TomNiklas.Markmann <tomniklas.markmann@akquinet.de>
2024-05-10 19:12:41 +0200

    Starting Screen not implemented yet || Sigin neu designed,  noch nciht gucken, wie es aussieht, avd geht nicht

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,50 +1,104 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, ActivityIndicator } from 'react-native'
-import { Button, Input } from 'react-native-elements'
-import { signInWithEmail } from '../User-Auth/auth'
+import React, { useState } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  Button,
+  TextInput,
+  Input,
+  Switch,
+  SafeAreaView,
+  Pressable,
+} from "react-native-elements";
+import { signInWithEmail } from "../User-Auth/auth";
 
 export default function SigninScreen() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(false)
-  
-    const handleSignIn = async () => {
-      setLoading(true)
-      const success = await signInWithEmail(email, password)
-      setLoading(false)
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [click, setClick] = useState(false);
+
+  const handleSignIn = async () => {
+    setLoading(true);
+    const success = await signInWithEmail(email, password);
+    setLoading(false);
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-          onChangeText={(text) => setEmail(text)}
+    <SafeAreaView>
+      <Text style={styles.title}>Login</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.input}
+          placeholder="EMAIL"
           value={email}
-          placeholder="email@address.com"
-          autoCapitalize={'none'}
+          onChangeText={setEmail}
+          autoCorrect={false}
+          autoCapitalize="none"
         />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
-          onChangeText={(text) => setPassword(text)}
+        <TextInput
+          style={styles.input}
+          placeholder="PASSWORD"
           value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={'none'}
+          onChangeText={setPassword}
+          autoCorrect={false}
+          autoCapitalize="none"
         />
       </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={handleSignIn} />
+      <View style={styles.rememberView}>
+        <View style={styles.switch}>
+          <Switch
+            value={click}
+            onValueChange={setClick}
+            trackColor={{ true: "green", false: "gray" }}
+          />
+          <Text style={styles.rememberText}>Remember Me</Text>
+        </View>
+        <View>
+          <Pressable onPress={() => Alert.alert("Forget Password!")}>
+            <Text style={styles.forgetText}>Forgot password?</Text>
+          </Pressable>
+        </View>
       </View>
-      {loading && (
-        <ActivityIndicator size="large" color="#0000ff" style={styles.loadingIndicator} />
-      )}
-    </View>
-  )
+      <View style={styles.container}>
+        <View style={[styles.verticallySpaced, styles.mt20]}>
+          <Input
+            label="Email"
+            leftIcon={{ type: "font-awesome", name: "envelope" }}
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            placeholder="email@address.com"
+            autoCapitalize={"none"}
+          />
+        </View>
+        <View style={styles.verticallySpaced}>
+          <Input
+            label="Password"
+            leftIcon={{ type: "font-awesome", name: "lock" }}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+            secureTextEntry={true}
+            placeholder="Password"
+            autoCapitalize={"none"}
+          />
+        </View>
+        <View style={[styles.verticallySpaced]}>
+          <Switch></Switch>
+        </View>
+        <View style={[styles.verticallySpaced, styles.mt20]}>
+          <Button title="Sign in" disabled={loading} onPress={handleSignIn} />
+        </View>
+        {loading && (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            style={styles.loadingIndicator}
+          />
+        )}
+        <View>
+          <Text>Test</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -55,9 +109,9 @@ const styles = StyleSheet.create({
   verticallySpaced: {
     paddingTop: 4,
     paddingBottom: 4,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
   },
   mt20: {
     marginTop: 20,
   },
-})
+});
---
+++ pages/StartingScreen.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { Button, Input } from "react-native-elements";
+
+export default function StartingScreen() {
+  <View>
+    
+  </View>;
+}
