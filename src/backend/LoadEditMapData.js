43697bce8b6c52fa5f8b7c68ed8600ec83a4dff6
lipaliga <l.paliga@stud.bhh.de>
2024-09-26 12:30:15 +0200

    moved favourite functions to LoadEditPlaceData.js

--- src/backend/LoadEditMapData.js
+++ src/backend/LoadEditMapData.js
@@ -1,5 +1,6 @@
 import { supabase } from '../services/supabase';
 import { Continent, Country, City, Place } from './MapClasses'; // Passe den Pfad zu deinen Klassen an
+import { isFavourite } from './LoadEditPlaceData';
 
 /**
 * Funktionen zum Verifizieren der besuchten Länder.
@@ -70,33 +71,6 @@ export const updateVisitedCountry = (location, findCountry, findNearestCity, use
   updateOrCreateVisitedCountry(country.countryId, userId);
 };
 
-
-/**
-* Funktionen zum Favorisieren der Orte.
-*
-*/
-const isFavourite = async (placeId, userId) => {
-  try {
-    // Überprüfe, ob ein Eintrag für die gegebene Attractions_ID und user_id existiert
-    const { data, error } = await supabase
-      .from('DesiredDestination')
-      .select('Attractions_ID')
-      .eq('Attractions_ID', placeId)
-      .eq('User_ID', userId)
-      .single();
-
-    if (error && error.code !== 'PGRST116') { // PGRST116: Single row expected, multiple rows found
-      throw error;
-    }
-
-    // Wenn ein Eintrag existiert, gib true zurück, ansonsten false
-    //console.log(!!data);
-    return !!data;
-  } catch (error) {
-    console.error('Fehler beim Überprüfen des Favoritenstatus:', error.message);
-    return false;
-  }
-};
 
 /**
 * Funktionen zum Fetchen aller existierender Länder, Städte, Orte aus der Datenbank.
