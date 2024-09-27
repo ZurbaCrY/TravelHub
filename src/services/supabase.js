b37e01ec1d04d837c21d0eb2cb7af19c1549cbec
ximragequit <freesearvid@gmail.com>
2024-09-27 10:33:49 +0200

    Revert "Alles fertig ausgelagert + Merge bereit"
    This reverts commit 426aeb749b7e4e7242a607da3ed9a182d8fdf350.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,6 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { SUPABASE_URL, SUPABASE_KEY } from '@env';
 
+console.log(SUPABASE_URL);
+
 const supabaseUrl = SUPABASE_URL;
 const supabaseKey = SUPABASE_KEY;
 
