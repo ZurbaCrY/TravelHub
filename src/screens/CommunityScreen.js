65d430ebc2fc3e5a07cc5bee9364e0081a0f12a1
ximragequit <freesearvid@gmail.com>
2024-09-27 11:53:26 +0200

    so endgültig fertig

--- src/screens/CommunityScreen.js
+++ src/screens/CommunityScreen.js
@@ -4,7 +4,8 @@ import { View, Text, StyleSheet, FlatList, TextInput, Image, TouchableOpacity } 
 import { useDarkMode } from '../context/DarkModeContext';
 import { supabase } from '../services/supabase';
 import * as ImagePicker from 'expo-image-picker';
-import AuthService from '../services/auth'
+import AuthService from '../services/auth';
+// import { SUPABASE_URL, SUPABASE_KEY } from '@env';
 
 export default function CommunityScreen() {
   const user = AuthService.getUser();
@@ -17,7 +18,6 @@ export default function CommunityScreen() {
     fetchPosts();
   }, []);
 
-  // Abrufen der geschriebenen Nachrichten
   const fetchPosts = async () => {
     try {
       const { data, error } = await supabase.from('posts').select('*').order('timestamp', { ascending: false });
@@ -31,14 +31,12 @@ export default function CommunityScreen() {
     }
   };
 
-  // Abschicken einer Nachricht
   const createNewPost = async () => {
     try {
       const { error } = await supabase.from('posts').insert([{ content: newPostContent, author: user_username, upvotes: 0, downvotes: 0 }]);
       if (error) {
         console.error('Error creating post:', error.message);
       } else {
-        console.log('Post created successfully');
         setNewPostContent('');
         fetchPosts();
       }
@@ -47,7 +45,6 @@ export default function CommunityScreen() {
     }
   };
 
-  // Upvote geben für eine Nachricht
   const handleUpvote = async (postId) => {
     try {
       const { data: postData, error } = await supabase.from('posts').select('upvotes', 'downvotes').eq('id', postId).single();
@@ -65,28 +62,17 @@ export default function CommunityScreen() {
     }
   };
 
-  // Downvote geben für eine Nachricht
   const handleDownvote = async (postId) => {
     try {
-      const { data: postData, error } = await supabase
-        .from('posts')
-        .select('downvotes')
-        .eq('id', postId)
-        .single();
-
+      const { data: postData, error } = await supabase.from('posts').select('downvotes').eq('id', postId).single();
       if (error) {
         throw error;
       }
       const updatedDownvotes = (postData.downvotes || 0) + 1;
-      const { error: updateError } = await supabase
-        .from('posts')
-        .update({ downvotes: updatedDownvotes })
-        .eq('id', postId);
-
+      const { error: updateError } = await supabase.from('posts').update({ downvotes: updatedDownvotes }).eq('id', postId);
       if (updateError) {
         throw updateError;
       }
-
       fetchPosts();
     } catch (error) {
       console.error('Error downvoting post:', error.message);
@@ -95,37 +81,30 @@ export default function CommunityScreen() {
 
   const handleImageUpload = async () => {
     try {
-      // Öffne den Dateiauswähler für den Benutzer
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
         aspect: [4, 3],
         quality: 1,
       });
-  
-      // Überprüfe, ob das Bild ausgewählt wurde und nicht abgebrochen wurde
+
       if (!result.canceled && result.assets.length > 0) {
         const firstAsset = result.assets[0];
         const fileUri = firstAsset.uri;
         const fileName = fileUri.substring(fileUri.lastIndexOf('/') + 1);
-  
-        // Lade das Bild hoch zu Supabase Storage
+
         const response = await fetch(fileUri);
         const blob = await response.blob();
-  
-        const { error } = await supabase
-          .storage
-          .from('Storage')
-          .upload(`images/${fileName}`, blob, {
-            cacheControl: '3600',
-            upsert: false,
-          });
-  
+
+        const { error } = await supabase.storage.from('Storage').upload(`images/${fileName}`, blob, {
+          cacheControl: '3600',
+          upsert: false,
+        });
+
         if (error) {
           throw error;
         }
-  
-        // Konstruiere den URI des hochgeladenen Bildes
+
         const imageUrl = `${process.env.REACT_APP_SUPABASE_URL}/storage/v1/object/public/Storage/images/${fileName}`;
         console.log('Image URL:', imageUrl);
       }
@@ -134,65 +113,117 @@ export default function CommunityScreen() {
     }
   };
 
-  // Elemente der Communityseite  
   return (
     <View style={[styles.container, { backgroundColor: isDarkMode ? '#070A0F' : '#FFF' }]}>
       <FlatList
         data={posts}
         renderItem={({ item }) => (
-          <View style={{ backgroundColor: isDarkMode ? '#374151' : '#E5E7EB', padding: 10, marginVertical: 5, width: 350 }}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5, alignItems: 'center' }}>
-              <Text style={{ color: isDarkMode ? '#FFF' : '#000' }}>{item.content}</Text>
-              <View style={{ flexDirection: 'row' }}>
-                <TouchableOpacity onPress={() => handleUpvote(item.id)} style={{ flexDirection: 'row', alignItems: 'center', marginRight: 10 }}>
-                  <Image source={require('../assets/images/thumbs-up.png')} style={{ width: 20, height: 20 }} />
-                  <Text style={{ color: isDarkMode ? '#FFF' : '#000', fontSize: 12, marginLeft: 3 }}>{item.upvotes}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => handleDownvote(item.id)} style={{ flexDirection: 'row', alignItems: 'center' }}>
-                  <Image source={require('../assets/images/thumbs-down.png')} style={{ width: 20, height: 20 }} />
-                  <Text style={{ color: isDarkMode ? '#FFF' : '#000', fontSize: 12, marginLeft: 3 }}>{item.downvotes}</Text>
-                </TouchableOpacity>
-              </View>
+          <View style={styles.postCard}>
+            {/* Post Header with Profile Image */}
+            <View style={styles.postHeader}>
+              <Image source={require('../assets/images/picture.png')} style={styles.profileImage} />
+              <Text style={styles.username}>{item.author}</Text>
             </View>
-            <Text style={{ color: isDarkMode ? '#9CA3AF' : '#6B7280', fontSize: 12 }}>{item.author}</Text>
+            {/* Post Content */}
+            <Image source={{ uri: item.image }} style={styles.postImage} />
+            <Text style={styles.postText}>{item.content}</Text>
+            {/* Post Footer with Like/Dislike */}
+            <View style={styles.postFooter}>
+              <TouchableOpacity onPress={() => handleUpvote(item.id)}>
+                <Image source={require('../assets/images/thumbs-up.png')} style={styles.icon} />
+              </TouchableOpacity>
+              <Text style={styles.upvoteText}>{item.upvotes}</Text>
+              <TouchableOpacity onPress={() => handleDownvote(item.id)}>
+                <Image source={require('../assets/images/thumbs-down.png')} style={styles.icon} />
+              </TouchableOpacity>
+              <Text style={styles.downvoteText}>{item.downvotes}</Text>
+            </View>
           </View>
         )}
         keyExtractor={item => item.id.toString()}
       />
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={[styles.input, { backgroundColor: isDarkMode ? '#374151' : '#E5E7EB', color: isDarkMode ? '#FFF' : '#000' }]}
+          style={styles.input}
           placeholder="Type here.."
           value={newPostContent}
           onChangeText={text => setNewPostContent(text)}
         />
         <TouchableOpacity onPress={handleImageUpload}>
-          <Image source={require('../assets/images/picture.png')} style={{ width: 50, height: 50 }} />
+          <Image source={require('../assets/images/picture.png')} style={styles.uploadIcon} />
         </TouchableOpacity>
         <TouchableOpacity onPress={createNewPost}>
-          <Image source={require('../assets/images/message_send.png')} style={{ width: 55, height: 55 }} />
+          <Image source={require('../assets/images/message_send.png')} style={styles.sendIcon} />
         </TouchableOpacity>
       </View>
     </View>
   );
 }
 
-// Design 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    paddingTop: 20,
-    justifyContent: 'flex-end', 
+    paddingTop: 10,
+  },
+  postCard: {
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginVertical: 10,
+    width: '95%',
+    padding: 15,
+  },
+  postHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  username: {
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  postImage: {
+    width: '100%',
+    height: 300,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+  postText: {
+    marginVertical: 5,
+  },
+  postFooter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  icon: {
+    width: 25,
+    height: 25,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '95%',
+    marginBottom: 20,
   },
   input: {
-    width: '70%',
+    flex: 1,
     height: 40,
-    borderWidth: 3,
-    borderRadius: 50,
+    backgroundColor: '#F1F1F1',
+    borderRadius: 20,
     paddingHorizontal: 10,
-    marginVertical: 10,
-    marginTop: 'auto',
-    borderColor: '#8a8a8a',
+    marginRight: 10,
+  },
+  uploadIcon: {
+    width: 40,
+    height: 40,
+  },
+  sendIcon: {
+    width: 40,
+    height: 40,
   },
 });
