5ade057457f6645fd0bc8cd63308ff50bb550795
TomNiklas.Markmann <tomniklas.markmann@akquinet.de>
2024-10-02 12:15:48 +0200

    API Key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,6 @@
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_URL, SUPABASE_KEY } from '@env';
-
-const supabaseUrl = SUPABASE_URL;
-const supabaseKey = SUPABASE_KEY;
 
 // Create a Supabase client instance with authentication settings
 export const supabase = createClient(
