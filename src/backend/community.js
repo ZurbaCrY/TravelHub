ea5eb99d184f221f675534844aef53b4b83e846f
ximragequit <freesearvid@gmail.com>
2024-10-13 19:18:11 +0200

    Pfp Wechsel fix

--- src/backend/community.js
+++ src/backend/community.js
@@ -508,7 +508,7 @@ export const handleFilePicker = async () => {
 export const handleNewProfilePicture = async (imageUrl) => {
   try {
     let uploadedImageUrl = null;
-    const CURRENT_USER = AuthService.getUser();
+    const CURRENT_USER = await AuthService.getUser();
     const CURRENT_USER_ID = CURRENT_USER.id;
 
     if (imageUrl) {
