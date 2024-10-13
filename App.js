335d79769199d7f90251f5ef30a57b10dae9ff3c
Tom-N-M <tom.niklas131@gmail.com>
2024-10-13 19:11:01 +0200

    Two little fixes

--- App.js
+++ App.js
@@ -12,7 +12,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import i18n from './src/assets/i18n/i18n';
 import { useTranslation } from 'react-i18next';
 
-
 export default function App() {
   return (
     <LoadingProvider>
@@ -53,8 +52,7 @@ function AppContent() {
         if (savedLanguage) {
           i18n.changeLanguage(savedLanguage);
         } else {
-          const locale = Localization.getLocales();
-          i18n.changeLanguage(locale);
+          i18n.changeLanguage('en');
         }
       } catch (error) {
         console.error('Error loading language:', error);
